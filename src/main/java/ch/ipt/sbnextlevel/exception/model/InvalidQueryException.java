64649183fa736aff70eaceb124a94040c6fbd7bc
julyvee <julyvee@web.de>
2020-08-30 11:46:01 +0200

    Ignore .factorypath file

--- src/main/java/ch/ipt/sbnextlevel/exception/model/InvalidQueryException.java
+++ src/main/java/ch/ipt/sbnextlevel/exception/model/InvalidQueryException.java
@@ -2,6 +2,10 @@ package ch.ipt.sbnextlevel.exception.model;
 
 public class InvalidQueryException extends Exception {
 
+    /**
+     *
+     */
+    private static final long serialVersionUID = -132517762686755407L;
     // TODO: Create a constructor that builds a custom error response
     private ErrorResponse errorResponse;
 
